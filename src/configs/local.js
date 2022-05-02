868d17a5778366b87ea9f3658d9354959f31472f
GigEama <56081818+GigEama@users.noreply.github.com>
2022-05-02 15:17:12 -0600

    me and honey changes

--- src/configs/local.js
+++ src/configs/local.js
@@ -3,5 +3,5 @@ module.exports={
     API_URL:'http://localhost:8080',
     statuses:['Pending','Paid','NotPaid'],
     countries:['Eritrea','Ethiopia','Canada','Uganda'],
-    doneby:'Amanuel'
+    doneby:'Amanuel and gherie'
 }
